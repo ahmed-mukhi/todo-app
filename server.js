d62d5f148cc536ee7ac088d1620ee7beadb4dc98
ahmed-mukhi <yaqoobdox.999@gmail.com>
2023-05-22 12:16:34 -0700

    static files served

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
+let path = require("path");
 const myRoutes = require("./routes/rootRouter");
 // const userRoutes = require("./routes/userRoutes");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (_, res) => {
+    res.sendFile(
+        path.join(__dirname, "./client/build/index.html"),
+        (err) => {
+            console.log(err);
+            res.status(500).send(err);
+        }
+    )
+});
 
 
 
@@ -16,22 +29,11 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(myRoutes);
 
 
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-app.get("*", (_, res) => {
-    res.sendFile(
-        path.join(__dirname, "./client/build/index.html"),
-        (err) => {
-            res.status(500).send(err);
-        }
-    )
-});
-
 mongoose.connect(process.env.MONG_URI).then(() => {
     try {
+        console.log("mongoose connnected");
         app.listen(process.env.PORT, () => {
             console.log("Listeneing on", process.env.PORT);
-            console.log("mongoose connnected");
         });
     } catch (error) {
         console.log(error)
