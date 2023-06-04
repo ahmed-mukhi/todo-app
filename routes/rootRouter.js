d2b396258b36bad77bad70f3b7985c676d2de1cc
ahmed-mukhi <yaqoobdox.999@gmail.com>
2023-06-04 04:29:25 -0700

    changed

--- routes/rootRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userRoutes = require("./userRoutes");
-const todoRoutes = require("./todosRouter");
-
-router.use("/todo", todoRoutes);
-router.use("/user", userRoutes);
-
-module.exports = router;
